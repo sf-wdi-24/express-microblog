d9985e43bfbd71095580c05c247a8b06d49d7d67
abeytaadam <measalion@gmail.com>
2015-11-13 15:29:30 -0800

    Add view engine and package accessibility

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+// Set up body-parser
+app.use(bodyParser.urlencoded({ extended : true }));
+
+// Set up static page directory
+app.use(express.static(__dirname + 'public'));
+
+// Set HBS view engine
+app.set('view engine', 'hbs');
+
+
+
+
+
+
 var server = app.listen(process.env.PORT || 3000, function(){
 	console.log('HEY! LISTEN!');
 });
