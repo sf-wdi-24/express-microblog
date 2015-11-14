88c1fecccc57340dd4c2b9e4ac3859d53899f76f
murcielago17 <murcielago17@hotmail.com>
2015-11-13 17:23:53 -0800

    initial commit

---
+++ public/main.js
@@ -0,0 +1,21 @@
+$(function() {
+  
+  // compile handlebars template
+  var source = $('#microblog-template').html();
+  var template = Handlebars.compile(source);
+
+  // array of test data
+  var allBlogPosts = [
+    { Post: 'BlogPost', category: 'Post' },
+    
+  ];
+
+  // AJAX call to GET all workouts
+  $.get('/api/blogPosts', function (data) {
+    allblogPosts = data.blogPosts;
+    
+    var blogPostsHtml = template({ blogPosts: allBlogPosts });
+    $('#blogPosts-list').append(blogPostsHtml);
+  });
+
+});
