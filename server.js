d0191d40d74c33461ec05da4b0096614d55ebbc8
tkhuynh <thequest2311@yahoo.com>
2015-11-13 16:36:59 -0800

    setup route to create new post

--- server.js
+++ server.js
@@ -12,7 +12,27 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "hbs");
 app.use(express.static("public"));
 
+var Post = require("./models/post");
+
+//set up posts api
+app.get("/api/posts", function (req, res) {
+	Post.find(function (err, allPosts) {
+		res.json(allPosts);
+	});
+});
+
+//create new post
+app.post("/api/posts", function (req, res) {
+	//create new post from form data
+	var newPost = new Post(req.body);
+	//save newPost in db
+	newPost.save(function (err, newPost) {
+		res.json(newPost);
+	});
+});
+
 //listen to port 3000
 var server = app.listen(process.env.PORT || 3000, function () {
 	console.log("I'm listening");
 });
+
