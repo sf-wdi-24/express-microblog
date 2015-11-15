b4e2f6735db4cbf99314d4dc06b69af44602a93e
TheEddyChang <ehchang58@yahoo.com>
2015-11-15 02:15:25 -0800

    added bootstrap css

--- public/main.js
+++ public/main.js
@@ -1,36 +1,31 @@
-$(function(){
-console.log("JS WORKING!");
-//handlebars set up for posts
-var baseUrl = '/api/posts';
-var $postslist = $('#posts-list');
-var source = $('#posts-template').html();
-var template = Handlebars.compile(source);
+$(function() {
+	console.log("JS WORKING!");
+	//handlebars set up for posts
+	var baseUrl = '/api/posts';
+	var $postslist = $('#posts-list');
+	var source = $('#posts-template').html();
+	var template = Handlebars.compile(source);
 
-var allPosts = [];
+	var allPosts = [];
 
-var render = function() {
-	// empty existing todos from view
-	$postslist.empty();
-
-	// pass `allTodos` into the template function
-	var postsHtml = template({
-		posts: allPosts //allTodos in mongo
-	});
-
-	// append html to the view
-	$postslist.append(postsHtml);
+	var render = function() {
+		// empty existing todos from view
+		$postslist.empty();
+		// pass `allTodos` into the template function
+		var postsHtml = template({
+			posts: allPosts //allTodos in mongo
+		});
+		// append html to the view
+		$postslist.append(postsHtml);
 	};
 
 
-$.get(baseUrl, function(data) {
-	console.log("hello");
-	//set allTodos to todo data from API
-	allPosts = data.posts;
-	render();
-});
-
-
-
+	$.get(baseUrl, function(data) {
+		console.log("hello");
+		//set allTodos to todo data from API
+		allPosts = data.posts;
+		render();
+	});
 	//   POSTING
 	$('.post-form').on('submit', function(event) {
 		event.preventDefault();
@@ -38,20 +33,12 @@ $.get(baseUrl, function(data) {
 
 		var newPost = $(this).serialize();
 		//POST request to create new book
-        $.post(baseUrl, newPost, function(data) {
+		$.post(baseUrl, newPost, function(data) {
 			console.log(data);
 			allPosts.push(data);
 			render();
 		});
-   });
-
-
-
-
-
-
-
-
+	});
 
 
 
