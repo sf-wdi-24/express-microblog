942104b17f34f9083ab35c634dcca4b61e61af78
DreTGiant <amassey30@yahoo.com>
2015-11-15 03:08:54 -0800

    post working

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,70 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	console.log('Ready!');
+
+	var source = $('#blog-template').html();
+	var template = Handlebars.compile(source);
+
+	var baseUrl = '/api/blogs';
+	var allBlogs = [];
+	// var $blogList = $('#blog-template');
+	var $blog_input = $('#blog-input');
+	var $blogList = $('#blogs-list');
+
+	// var allBlogs = [{
+	// 	place: 'Berlin',
+	// 	description: 'my friends and i in berlin',
+	// 	image:'http://www.tonedeaf.com.au/wp-content/uploads/2013/03/berlin-germany-copy.jpg'
+	// }];
+	var blogsHtml = template({
+		blog: allBlogs
+	});
+
+	$('#blogs-list').append(blogsHtml);
+
+
+
+	var render = function() {
+		// empty existing todos from view
+		$blogList.empty();
+		// pass `allTodos` into the template function
+		var blogsHtml = template({
+			blog: allBlogs
+		});
+		// append html to the view
+		$blogList.append(blogsHtml);
+	};
+
+	// GET all blogs on page load
+	$.get(baseUrl, function(data) {
+		// set `allblogs` to blogs data from API
+		allBlogs = data.blog;
+		console.log(data.blog);
+
+		$blogList.append(template({
+			blog: allBlogs
+		}));
+	});
+
+      //Add new task
+  $blog_input.on('submit', function(event) {
+    event.preventDefault();
+    // serialze form data
+    var newBlog = $(this).serialize();
+    // POST request to create new book
+    $.post(baseUrl, newBlog, function(data) {
+      console.log(data);
+      // add new book to `allBooks`
+      allBlogs.push(data);
+      $blogList.append(template({
+        blog: allBlogs
+      }));
+
+      render();
+
+    });
+    $blog_input[0].reset();
+    $blog_input.find('input').first().focus();
+  });
+
+
 });
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
-mongoose = require('mongoose');
-bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var blog = require('./models/blog');
 
 var exphbs  = require('hbs');
 app.set('view engine', 'hbs');
@@ -13,7 +14,48 @@ mongoose.connect('mongodb://localhost/blog');
 
 var Blog = require('./models/blog');
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'));
+
+
+
+
+
+// Get ALl Task 
+app.get('/api/blogs', function(req, res) {
+	//mongoose
+	blog.find(function(err, allBlogs) {
+		res.json({
+			blog: allBlogs
+		}); 											
+	});
+
+});
+
+	//Post single task
+	app.post('/api/blogs/', function(req, res) {
+		//New blog with form data
+		var newBlog = new Blog(req.body); 
+		newBlog.save(function(err, savedBlog) {
+			res.json(newBlog)
+		});
+	});
+
+
+// delete todo
+app.delete('/api/blogs/:id', function (req, res) {
+  // get blog id from url params (`req.params`)
+  var blogId = req.params.id;
+
+  // find blog in db by id and remove
+  blog.findOneAndRemove({ _id: blogId }, function (err, deletedBlog) {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.json(deletedBlog);
+    }
+  });
+});
+
 
 // Homepage route
 app.get('/', function(req, res) {
