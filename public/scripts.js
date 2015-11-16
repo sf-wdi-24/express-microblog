5c763b19617e9a5de5017e65000effa4e1f387d9
abeytaadam <measalion@gmail.com>
2015-11-15 19:50:25 -0800

    POST and PUT functionality

--- public/scripts.js
+++ public/scripts.js
@@ -1,15 +1,83 @@
 $(document).ready(function (){
-console.log('Ready');
+	console.log('Ready');
 
-// Compile handlebars
-var source = $('#blog-template').html();
-var template = Handlebars.compile(source);
+	var id;
 
-$.get('/api/blogs', function (data){
-	console.log(data);
-	var blogList = data.blogs;
-	blogArr = blogList;
-});
+	// Compile handlebars
+	var source = $('#blog-template').html();
+	var template = Handlebars.compile(source);
+
+	var blogArr = [];
+	console.log('BLOG ARRAY', blogArr);
+	var $blogBody = $('#blog-body');
+
+	function addBlogpost() {
+		$blogBody.empty();
+		var blogHtml = template({
+			posts: blogArr
+		});
+		$blogBody.append(blogHtml);
+	}
+
+// GET all blogposts
+	$.get('/api/posts', function (data) {
+		console.log(data);
+		console.log('DATA.BLOGS', data.posts);
+		blogArr = data.posts;
+		addBlogpost();
+	});
+
+	$(document).on('click', '.glyphicon-pencil', function (event) {
+				event.preventDefault();
+				id = $(this).attr('id');
+				console.log(id);
+				$('.glyphicon-remove').toggle();
+
+				console.log(this);
+			});
+
+	// POST new blogpost 
+	$('#newBlogForm').on('submit', function (event) {
+		event.preventDefault();
+		var newBlogpost = $(this).serialize();
+
+		$.post('/api/posts', newBlogpost, function (data) {
+			console.log('POST DATA', data);
+
+			blogArr.push(data);
+
+			addBlogpost();
+		});
+
+		document.getElementById('newBlogForm').reset();
+	});
+
+// PUT (edit) blogpost
+	$(document).on('submit', '.submitEdit', function (event) {
+		event.preventDefault();
+		var postId = $(this).closest('.pull-left').attr('id');
+		console.log("THIS", this);
+		console.log("POST ID", id);
+
+		var postToUpdate = blogArr.filter(function (post){
+			return post._id == postId;
+		})[0];
+
+		var updatedBlogpost = $(this).serialize();
+
+		$.ajax({
+			type: 'PUT',
+			url: '/api/posts' + '/' + id,
+			data: updatedBlogpost,
+			success: function(data) {
+				blogArr.splice(blogArr.indexOf(postToUpdate), 1, data);
+				console.log()
+				addBlogpost();
+			}
+		});
+	});
+
+
 
 
 });
--- server.js
+++ server.js
@@ -21,12 +21,42 @@ app.get('/', function (req, res) {
 	res.render('index');
 });
 
-app.get('/api/blogs', function (req, res){
+
+// GET all blogposts
+app.get('/api/posts', function (req, res){
 	Blogpost.find(function (err, allBlogposts){
 		res.json({ posts : allBlogposts});
 	});
 });
 
+// GET one blogpost
+app.get('/api/posts/:id', function (req, res){
+	var postId = req.params.id;
+	Blogpost.findOne({ _id: postId}, function (err, foundBlogpost){
+		res.json(foundBlogpost);
+	});
+});
+
+// POST new blogpost
+app.post('/api/posts', function (req, res) {
+	var newBlogpost = new Blogpost(req.body);
+	newBlogpost.save(function (err, savedBlogpost) {
+		res.json(savedBlogpost);
+	});
+});
+
+// PUT (edit) blogpost
+app.put('/api/posts/:id', function (req, res){
+	var postId = req.params.id;
+	Blogpost.findOne({_id: postId}, function (err, foundBlogpost){
+		foundBlogpost.title = req.body.title;
+		foundBlogpost.content = req.body.content;
+
+		foundBlogpost.save(function (err, savedBlogpost){
+			res.json(savedBlogpost);
+		});
+	});
+});
 
 
 // Server listening?
