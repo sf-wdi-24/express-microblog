8e1b735b3fb8a01e8d29733fe5b1a05714cecc8e
stanleyyork <stanleyyork@gmail.com>
2015-11-15 17:03:31 -0800

    added a navbar, some more css

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -5,6 +5,8 @@ $(function() {
  	var template = Handlebars.compile(source);
 
  	getAllPosts();
+ 	showFaves($('#updateFavorite').val());
+
 
 
  	// Add Single Post
@@ -22,17 +24,19 @@ $(function() {
 		var title = $('#updateTitle').val();
 		var body = $('#updateBody').val();
 		var url = $('#updateURL').val();
-		var favorite = $('#updateFavorite').val();
-		console.log(id+title+body+url+favorite);
+		var favorite = '';
+		if ($('#updateFavorite').prop("checked") === true){
+			favorite = true;
+		} else {
+			favorite = false;
+		}
 		updatePost(id, title, body, url, favorite);
 	});
 
 	// Delete Single Post
 	$('#delete-post-form').on('click', function(e){
 		e.preventDefault();
-		console.log("inside delete");
 		var id = $('#delete-post-form').attr('data-id');
-		console.log(id);
 		var answer = prompt("Are you sure you want to delete this? (Yes/No)");
 		if(answer.toLowerCase() === "yes" || answer.toLowerCase() === "y" ){
 			deletePost(id);
@@ -48,6 +52,13 @@ $(function() {
 			$('#total-count-badge').text(totalposts);
 			var postsHtml = template({ posts: allPosts });
 			$('#posts-list').append(postsHtml);
+			for(var i = 0; i<allPosts.length; i++){
+				if(allPosts[i].favorite === true){
+					$('#post-favorite-'+allPosts[i]._id).show();
+				} else {
+					$('#post-favorite-'+allPosts[i]._id).hide();
+				}
+			}
 		});
 	}
 
@@ -69,14 +80,13 @@ $(function() {
 
 	// updatePost function - calls API
 	function updatePost(id, title, body, url, favorite){
-		console.log(title);
 		$.ajax({
 			type: "PUT",
 			url: 'http://localhost:3000/api/posts/' + id,
 			data: {title: title, body: body, url: url, favorite:favorite},
 			success: function (data) {
 				console.log("Updated post!");
-				getAllPosts();
+				window.location.href = "http://localhost:3000/posts/"+id;
 			},
 			error: function (error) {
 				console.error(error);
@@ -98,5 +108,15 @@ $(function() {
 	      	}
 		});
 	}
+
+	function showFaves(favorite){
+		if(favorite === "true"){
+			$('#post-favorite').show();
+		} else {
+			$('#post-favorite').hide();
+		}
+	}
+
+
 
 });
