fe5b76fc9949aed28f1049106d9e9b8b63f947c5
DreTGiant <amassey30@yahoo.com>
2015-11-18 09:00:49 -0800

    w4d3hw

--- server.js
+++ server.js
@@ -1,55 +1,95 @@
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var blog = require('./models/blog');
+var express = require('express'),
+  app = express(),
+  mongoose = require('mongoose'),
+  bodyParser = require('body-parser'),
+  blog = require('./models/blog');
 
-var exphbs  = require('hbs');
+var Blog = require('./models/blog');
+//var Post = require('./models/post');
+var User = require('./models/user');
+
+
+var cookieParser = require('cookie-parser'),
+  session = require('express-session'),
+  passport = require('passport'),
+  LocalStrategy = require('passport-local').Strategy;
+
+
+// middleware for auth
+app.use(cookieParser());
+app.use(session({
+  secret: 'supersecretkey',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// passport config
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+var exphbs = require('hbs');
 app.set('view engine', 'hbs');
 
 app.use(bodyParser.urlencoded({
-	extended: true
+  extended: true
 }));
 mongoose.connect('mongodb://localhost/blog');
 
-var Blog = require('./models/blog');
+
 
 app.use(express.static(__dirname + '/public'));
 
 
 
-
+app.post('/signup', function(req, res) {
+  User.register(new User({
+      username: req.body.username
+    }), req.body.password,
+    function(err, newUser) {
+      passport.authenticate('local')(req, res, function() {
+        res.send('signed up!!!');
+      });
+    }
+  );
+});
 
 // Get ALl Blogs 
 app.get('/api/blogs', function(req, res) {
-	//mongoose
-	blog.find(function(err, allBlogs) {
-		res.json({
-			blog: allBlogs
-		}); 											
-	});
+  //mongoose
+  blog.find(function(err, allBlogs) {
+    res.json({
+      blog: allBlogs
+    });
+  });
 
 });
 
-	//Post single blog
-	app.post('/api/blogs/', function(req, res) {
-		//New blog with form data
-		var newBlog = new Blog(req.body); 
-		newBlog.save(function(err, savedBlog) {
-			res.json(newBlog)
-		});
-	});
+//Post single blog
+app.post('/api/blogs/', function(req, res) {
+  //New blog with form data
+  var newBlog = new Blog(req.body);
+  newBlog.save(function(err, savedBlog) {
+    res.json(newBlog)
+  });
+});
 
 
 // delete blog
-app.delete('/api/blogs/:id', function (req, res) {
+app.delete('/api/blogs/:id', function(req, res) {
   // get blog id from url params (`req.params`)
   var blogId = req.params.id;
 
   // find blog in db by id and remove
-  blog.findOneAndRemove({ _id: blogId }, function (err, deletedBlog) {
+  blog.findOneAndRemove({
+    _id: blogId
+  }, function(err, deletedBlog) {
     if (err) {
-      res.status(500).json({ error: err.message });
+      res.status(500).json({
+        error: err.message
+      });
     } else {
       res.json(deletedBlog);
     }
@@ -59,22 +99,28 @@ app.delete('/api/blogs/:id', function (req, res) {
 
 // update task
 
-app.put('/api/blogs/:id', function (req, res) {
+app.put('/api/blogs/:id', function(req, res) {
   var blogId = req.params.id;
 
-  blog.findOne({ _id: blogId }, function (err, foundBlog) {
+  blog.findOne({
+    _id: blogId
+  }, function(err, foundBlog) {
     if (err) {
-      res.status(500).json({ error: err.message });
+      res.status(500).json({
+        error: err.message
+      });
     } else {
-     
+
       foundBlog.place = req.body.place;
       foundBlog.description = req.body.description;
       foundBlog.image = req.body.image;
 
       // save updated blog in db
-      foundBlog.save(function (err, savedBlog) {
+      foundBlog.save(function(err, savedBlog) {
         if (err) {
-          res.status(500).json({ error: err.message });
+          res.status(500).json({
+            error: err.message
+          });
         } else {
           res.json(savedBlog);
         }
@@ -83,12 +129,29 @@ app.put('/api/blogs/:id', function (req, res) {
   });
 });
 
+// show user profile page
+app.get('/profile', function (req, res) {
+  res.render('profile', { user: req.user });
+});
+// log in user
+app.post('/login', passport.authenticate('local'), function (req, res) {
+  res.send('logged in!!!');
+});
+
+// show login view
+app.get('/login', function (req, res) {
+  res.render('login');
+});
+// show signup view
+app.get('/signup', function(req, res) {
+  res.render('signup');
+});
 
 // Homepage route
 app.get('/', function(req, res) {
-	res.render('index');
+  res.render('index');
 });
 
 var server = app.listen(process.env.PORT || 3000, function() {
-	console.log('Hello...Is it me your looking for..')
+  console.log('Hello...Is it me your looking for..')
 });
