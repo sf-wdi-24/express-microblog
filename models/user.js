fc178a4a996a016e75e446be84b4b3ffbda233a2
mrockway <mrockway@gmail.com>
2015-11-17 16:13:55 -0800

    user auth setup

---
+++ models/user.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose'),
+		Schema = mongoose.Schema,
+		passportLocalMongoose = require('passport-local-mongoose');
+
+var userSchema = new Schema({
+	username: String,
+	password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+var User = mongoose.model('User', userSchema);
+module.exports = User;
--- server.js
+++ server.js
@@ -3,13 +3,34 @@ var express = require('express');
 var hbs = require('hbs');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+// require models
 var Blog = require('./models/blog');
 var Comment = require('./models/comment');
+var User = require('./models/user');
 
 //connect mongo database to project
 mongoose.connect('mongodb://localhost/blogs_app');
 
 var app = express();
+
+//tell express to use passport
+app.use(cookieParser());
+app.use(session({
+	secret: 'supersecretkey',
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //tell express to use public dir for css and js
 app.use(express.static(__dirname + '/public'));
@@ -91,16 +112,53 @@ app.post('/api/blogs/:id/comments/', function (req,res){
 });
 
 //update individual comment
-app.put('/api/blogs/:id/comments/:comment-id', function(req,res){
-	var blogId = req.params.id;
-	var commentId = req.params.commentId;
-	Comment.findOne({_id : commentId}, function(err, foundComment){
-		updatedComment.text = req.body.text;
-		updatedComment.save();
-		res.json(updatedComment);
+// app.put('/api/blogs/:id/comments/:comment-id', function(req,res){
+// 	var blogId = req.params.id;
+// 	var commentId = req.params.comment-id;
+// 	Comment.findOne({_id : comment-id}, function(err, foundComment){
+// 		updatedComment.text = req.body.text;
+// 		updatedComment.save();
+// 		res.send(err);
+// 		res.json(updatedComment);
+// 	});
+// });
+
+
+//signin GET route for new users
+app.get('/signup', function (req,res) {
+	res.render('signup');
+});
+
+//sign up a new user and log them in
+app.post('/signup', function (req,res) {
+	User.register(new User({ username: req.body.username}), req.body.password, 
+		function (err, newUser) { 
+		passport.authenticate('local')(req,res, function(){
+			res.redirect('/profile');
+		});
 	});
 });
 
+//show login page for an existing user
+app.get('/login', function (req,res) {
+	res.render('login');
+});
+
+//post route for login user
+app.post('/login', passport.authenticate('local'), function (req,res) {
+	res.redirect('/profile');
+});
+
+//log out user
+app.get('/logout', function (req,res) {
+	req.logout();
+	res.redirect('/');
+});
+
+//user profile page
+app.get('/profile', function (req,res) {
+	res.render('profile', { user: req.user });
+});
 
 //set express to use localport
 app.listen(3000, function(){
